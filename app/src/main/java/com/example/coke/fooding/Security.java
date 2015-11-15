9d0b430bc93c92f3073493e51114ae2f7722be0a
Jaime <jai_mi_to@hotmail.es>
2015-11-15 21:06:39 +0100

    Implemented encrypt_password method in Security class

--- app/src/main/java/com/example/coke/fooding/Security.java
+++ app/src/main/java/com/example/coke/fooding/Security.java
@@ -5,18 +5,31 @@ package com.example.coke.fooding;
 
 public class Security {
 
-    /**
-     * Clase para encriptar passwords
-     * @version 1.0
-     *  - stub method
-     * @date 14/11/2015
-     */
+	/**
+	 * Clase para encriptar passwords
+	 * 
+	 * @version 1.0 - stub method
+	 * @date 14/11/2015
+	 */
 
-    /**
-     * @param pw: password a encriptar
-     * @return una cadena de caracteres que representa la password (encriptada)
-     */
-    public static String encrypt_password(String pw){
-        return pw;
-    }
+	/**
+	 * @param pw:
+	 *            password a encriptar
+	 * @return una cadena de caracteres que representa la password (encriptada)
+	 */
+	public static String encrypt_password(String pw) {
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(pw.getBytes());
+
+		byte byteData[] = md.digest();
+
+		// Convertir los bytes a hexadecimal
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		return sb.toString();
+	}
 }
