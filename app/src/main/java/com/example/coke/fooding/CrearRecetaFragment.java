9166faa683756317d5deb87bdc87b1d011d51b1e
cristinalahoz <544393@unizar.es>
2015-12-15 21:14:15 +0100

    Crear receta añadiendo solo 3 ingredientes

--- app/src/main/java/com/example/coke/fooding/CrearRecetaFragment.java
+++ app/src/main/java/com/example/coke/fooding/CrearRecetaFragment.java
@@ -1,6 +1,9 @@
 package com.example.coke.fooding;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +17,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.coke.fooding.data.Ingrediente;
+import com.example.coke.fooding.data.Receta;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -25,16 +29,34 @@ import java.util.List;
 public class CrearRecetaFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
 
     View view;
-    private EditText ingredientesReceta1;
-    private EditText ingredientesReceta2;
-    private EditText ingredientesReceta3;
     private EditText nombreReceta;
     private EditText elaboracionReceta;
 
     private Spinner tipoReceta;
-    private ArrayAdapter<String> adaptadorType;
     public static List<String> tipos = new LinkedList<String>();
     final Spinner tipoInputSpinner=null;
+
+    public static List<String> tiposIngredientes = new LinkedList<String>();
+
+    private LinearLayout layout1;
+    private LinearLayout layout2;
+    private LinearLayout layout3;
+
+    private EditText cant1;
+    private EditText cant2;
+    private EditText cant3;
+
+    private EditText uds1;
+    private EditText uds2;
+    private EditText uds3;
+
+    private Spinner tipoInputSpinnerIngredientes;
+    private Spinner tipoInputSpinnerIngredientes2;
+    private Spinner tipoInputSpinnerIngredientes3;
+
+
+
+
 
     public static boolean test = false;
 
@@ -42,19 +64,17 @@ public class CrearRecetaFragment extends android.support.v4.app.Fragment impleme
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
-
-
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.crear_receta, container, false);
 
         Button confirmButton = (Button) view.findViewById(R.id.CrearRecetaButton);
         confirmButton.setOnClickListener(this);
 
-        ingredientesReceta1 = (EditText) view.findViewById(R.id.ingredientes);
-        elaboracionReceta = (EditText) view.findViewById(R.id.elaboracion);
+        //Añadir nombre de la receta
         nombreReceta = (EditText) view.findViewById(R.id.nombreReceta);
-        //tipoReceta = (EditText) view.findViewById(R.id.tipoReceta);
 
+
+        // Añadir tipo de la receta
         tipos.add("Seleccione tipo");
         tipos.addAll(ClientInterface.getTipos());
 
@@ -63,47 +83,64 @@ public class CrearRecetaFragment extends android.support.v4.app.Fragment impleme
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String> (this.getActivity(), android.R.layout.simple_spinner_dropdown_item, tiposRecetas);
         tipoInputSpinner.setAdapter(adapter2);
 
+        //Añadir ingredientes
+        tiposIngredientes.add("Seleccione ingrediente");
+        tiposIngredientes.addAll(ClientInterface.getIngredientes());
+
+        layout1 = (LinearLayout) view.findViewById(R.id.layout);
+        layout2 = (LinearLayout) view.findViewById(R.id.layout2);
+        layout3 = (LinearLayout) view.findViewById(R.id.layout3);
+
+        //elegir un ingrediente de la bd
+        tipoInputSpinnerIngredientes = (Spinner) view.findViewById(R.id.TextoIng);
+        tipoInputSpinnerIngredientes2 = (Spinner) view.findViewById(R.id.TextoIng2);
+        tipoInputSpinnerIngredientes3 = (Spinner) view.findViewById(R.id.TextoIng3);
+
+        //cantidad de ingrediente
+        cant1 = (EditText) view.findViewById(R.id.cant);
+        cant2 = (EditText) view.findViewById(R.id.cant2);
+        cant3 = (EditText) view.findViewById(R.id.cant3);
+
+        //unidades de ingrediente
+        uds1 = (EditText) view.findViewById(R.id.uds);
+        uds2 = (EditText) view.findViewById(R.id.uds2);
+        uds3 = (EditText) view.findViewById(R.id.uds3);
+
+        String[] tiposIngredientes2 = tiposIngredientes.toArray(new String[tipos.size()]);
+        ArrayAdapter<String> adapterIngre = new ArrayAdapter<String> (this.getActivity(), android.R.layout.simple_spinner_dropdown_item, tiposIngredientes2);
+        tipoInputSpinnerIngredientes.setAdapter(adapterIngre);
+        tipoInputSpinnerIngredientes2.setAdapter(adapterIngre);
+        tipoInputSpinnerIngredientes3.setAdapter(adapterIngre);
 
 
+        final Button botonAnadeIngrediente = (Button) view.findViewById(R.id.mas_ingredientes);
+        botonAnadeIngrediente.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (layout1.getVisibility() == View.GONE) {
+                    layout1.setVisibility(View.VISIBLE);
+                } else if (layout2.getVisibility() == View.GONE) {
+                    layout2.setVisibility(View.VISIBLE);
+                } else {
+                    layout3.setVisibility(View.VISIBLE);
+                    botonAnadeIngrediente.setText("Maximo numero de Ingredientes");
+                }
+            }
+        });
+
+        //Elaboracion de la receta
+        elaboracionReceta = (EditText) view.findViewById(R.id.elaboracion);
 
         return view;
 
     }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.CrearRecetaButton:
 
-                String valueTipo = MainActivity.saberTipo(tipoInputSpinner.getSelectedItemPosition());
-                List<Ingrediente> listaIngredientes = new LinkedList<Ingrediente>();
-              /**  if(ingredientesReceta1.getVisibility() == View.VISIBLE){
-
-                    //listaIngredientes.add(ingredientesReceta1.getText());
-                }*/
-               // boolean creado = ClientInterface.crear_receta(nombreReceta, valueTipo, elaboracionReceta, )
-               // boolean creado = ClientInterface.crear_usuario(mEmailView.getText().toString(), mNombreView.getText().toString(), mPasswordView.getText().toString(), test);
-              //  Toast.makeText(getActivity(), "Email: " + mEmailView.getText() + " Pass: " + mPasswordView.getText() + " Nombre: " + mNombreView.getText(), Toast.LENGTH_SHORT).show();
-              /**  if(creado){
-                    Toast.makeText(getActivity(), "Se ha enviado un e-mail de verificación al correo introducido",Toast.LENGTH_SHORT ).show();
-                }else{
-                    Toast.makeText(getActivity(), "Usuario no registrado",Toast.LENGTH_SHORT).show();
-                }*/
                 break;
-            case R.id.angry_btn:
-                final Button botonAnadeIngrediente = (Button) view.findViewById(R.id.angry_btn);
-                if (ingredientesReceta1.getVisibility() == View.GONE){
-                    ingredientesReceta1.setVisibility(View.VISIBLE);
-                }
-                else if (ingredientesReceta2.getVisibility() == View.GONE){
-                    ingredientesReceta2.setVisibility(View.VISIBLE);
-                }
-                else{
-                    ingredientesReceta3.setVisibility(View.VISIBLE);
-                    botonAnadeIngrediente.setText("Maximo numero de Ingredientes");
-                }
-
-
         }
     }
 
